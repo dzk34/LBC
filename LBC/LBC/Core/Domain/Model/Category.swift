c8797d48bdfe206110b8520cf21de95d85da765e
dzk34 <khaledus34@gmail.com>
2024-05-29 16:17:58 +0100

    Added the Category & ClassifiedAd models

---
+++ LBC/LBC/Core/Domain/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  LBC
+//
+//  Created by Khaled on 29/05/2024.
+//
+
+import Foundation
+
+struct Category: Codable {
+    let id: Int
+    let name: String
+}
---
+++ LBC/LBC/Core/Domain/Model/ClassifiedAd.swift
@@ -0,0 +1,34 @@
+//
+//  ClassifiedAd.swift
+//  LBC
+//
+//  Created by Khaled on 29/05/2024.
+//
+
+import Foundation
+
+struct ClassifiedAd: Codable {
+    let id: Int
+    let title: String
+    let categoryId: Int
+    let creationDate: String // format: "yyyy-MM-dd'T'HH:mm:ssZ"
+    let description: String
+    let imagesUrl: ImagesURL
+    let isUrgent: Bool
+    let price: CGFloat
+    let siret: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, description, price, siret
+        case categoryId = "category_id"
+        case creationDate = "creation_date"
+        case imagesUrl = "images_url"
+        case isUrgent = "is_urgent"
+    }
+}
+
+
+struct ImagesURL: Codable {
+    let small: String? //add placeholder?
+    let thumb: String?
+}
